6f5db0d3d2a5186223d2c9f299c7f91ce3eb2242
Yu <yw3028@tc.columbia.edu>
2020-10-12 10:00:07 +0200

    update models

--- server/src/index.js
+++ server/src/index.js
@@ -3,7 +3,7 @@ import Express, { json } from 'express';
 import { config } from 'dotenv';
 import cors from 'cors';
 
-import { db } from './models';
+import { dbConfig } from './models';
 
 import router from './router';
 
@@ -18,10 +18,10 @@ app.use(router);
 
 (async () =>{
   try {
-    await db.sequelize.sync();
+    await dbConfig.sequelize.sync();
     app.listen(PORT);
     console.log(`Server listening on port ${PORT}`); // eslint-disable-line no-console
-  } catch (e) {
-    console.error('Error connecting to the db', e); // eslint-disable-line no-console
+  } catch (error) {
+    console.error('Error connecting to the database', error); // eslint-disable-line no-console
   }
 })();
--- server/src/models/index.js
+++ server/src/models/index.js
@@ -1,7 +1,4 @@
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const basename = path.basename(__filename);
+import * as sequelize from 'sequelize';
 
 import { config } from 'dotenv';
 config();
@@ -10,38 +7,15 @@ const dbName = process.env.DB_NAME || '';
 const dbUser = process.env.DB_USER || '';
 const dbPassword = process.env.DB_PASSWORD || '';
 
-const db = {};
-
-let sequelize = new Sequelize(dbName, dbUser, dbPassword, {
+export const dbConfig = new sequelize.Sequelize(dbName, dbUser, dbPassword, {
   port: Number(process.env.DB_PORT),
   host: process.env.DB_HOST,
-  logging: false,
+  dialect: 'postgres',
   pool: {
-    max: 5,
     min: 0,
-    acquire: 30000,
-    idle: 10000
+    max: 5,
+    acquire: 30000000,
+    idle: 10000000,
   },
-  operatorsAliases: false 
+  logging: false,
 });
-
-fs
-  .readdirSync(__dirname)
-  .filter(file => {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
-Object.keys(db).forEach(modelName => {
-  if (db[modelName].associate) {
-    db[modelName].associate(db);
-  }
-});
-
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
