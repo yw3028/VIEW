3aa78f8fcc94e950d57f08f7e74a6c45b4149121
Yu <yw3028@tc.columbia.edu>
2020-10-15 18:32:18 +0200

    push for merge

--- server/src/index.js
+++ server/src/index.js
@@ -31,7 +31,7 @@ app.use('/journal', journalRouter);
 (async () => {
   try {
     await sequelize.authenticate();
-    await sequelize.sync();
+    await sequelize.sync({ force: true });
     console.log('Connected to the database'); // eslint-disable-line no-console
     await app.listen(PORT);
     console.log(`Server listening on port ${PORT}`); // eslint-disable-line no-console
