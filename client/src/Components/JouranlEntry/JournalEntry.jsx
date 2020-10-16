6fbb5920f5b0db0d2202a47b88ab1fc4a1b9a7fd
Yu <yw3028@tc.columbia.edu>
2020-10-16 16:51:36 +0200

    feat: pass journal prop

--- client/src/Components/JouranlEntry/JournalEntry.jsx
+++ client/src/Components/JouranlEntry/JournalEntry.jsx
@@ -1,31 +1,18 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import moment from 'moment';
 import * as S from './JournalEntryStyle';
 
-import { getJournalById } from '../../Services/apiClient';
-
-const JournalEntry = (props) => {
-  const journalId = props.match.params.id;
-  const [journalEntry, setJournalEntry] = useState({});
-
-  useEffect(() => {
-    getJournalById(journalId).then((journal) => {
-      setJournalEntry(journal);
-    });
-  }, []);
+const JournalEntry = ({journalEntry}) => {
 
   return (
     <S.JournalEntryContainer>
-      {console.log(journalEntry)}
-      {/* <S.Test> */}
       <S.DateWeatherContainer>
         <p>{moment(journalEntry.date).format('MMMM Do YYYY, h:mm')}</p>
         <S.MobileLabel>&nbsp;·&nbsp;</S.MobileLabel>
-        <p>{journalEntry.location} · 22ºC</p>
+        <p>{journalEntry.location} · {journalEntry.weatherTemperature}ºC</p>
       </S.DateWeatherContainer>
-      <S.JournalTitle>{journalEntry.title}</S.JournalTitle>
-      <S.JournalContent>{journalEntry.entry}</S.JournalContent>
-      {/* </S.Test> */}
+      <S.JournalTitle value={journalEntry.title}>{journalEntry.title}</S.JournalTitle>
+      <S.JournalContent value={journalEntry.entry}>{journalEntry.entry}</S.JournalContent>
     </S.JournalEntryContainer>
   );
 };
