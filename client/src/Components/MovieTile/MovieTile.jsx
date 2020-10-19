8c06f0b0daffbab291ad243627ed4d9c2839c020
Yu <yw3028@tc.columbia.edu>
2020-10-19 21:22:07 +0200

    fix: pass movie instead movieId as prop to the buttons

--- client/src/Components/MovieTile/MovieTile.jsx
+++ client/src/Components/MovieTile/MovieTile.jsx
@@ -2,41 +2,21 @@ import React, { useContext } from 'react';
 import Button from '../ActionButton/ActionButtons';
 import { MovieContext } from '../../App';
 import { BrowserRouter as Router, Link, Route } from 'react-router-dom';
-import { useSpring, animated } from 'react-spring';
 
 import * as S from './MovieTileStyle';
 
 export default ({ movie }) => {
   const { updateMovieStatusInList } = useContext(MovieContext);
-  const calc = (x, y) => [
-    -(y - window.innerHeight / 2) / 20,
-    (x - window.innerWidth / 2) / 20,
-    1.1,
-  ];
-  const trans = (x, y, s) =>
-    `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`;
-  const [props, set] = useSpring(() => ({
-    xys: [0, 0, 1],
-    config: { mass: 5, tension: 350, friction: 40 },
-  }));
-
   return (
-        <animated.div
-          onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
-          onMouseLeave={() => set({ xys: [0, 0, 1] })}
-          style={{ transform: props.xys.interpolate(trans) }}
-        >
     <S.MovieTile>
       <Link to={`/movie/${movie.apiId ? movie.apiId : movie.id}`}>
-      <div>
-
+        <div>
           <img
             className="movie_img"
             alt="movie cover"
             src={'https://image.tmdb.org/t/p/w300/' + movie.poster_path}
           />
-      </div>
-        
+        </div>
       </Link>
 
       <div className="movie_overlay">
@@ -44,7 +24,7 @@ export default ({ movie }) => {
           <Button
             color={'white'}
             wish={true}
-            movieId={movie.apiId ? movie.apiId : movie.id}
+            movie={movie}
             onClick={(event) => {
               event.preventDefault();
               // event.stopPropagation();
@@ -59,11 +39,10 @@ export default ({ movie }) => {
           <Button
             color={'white'}
             watched={true}
-            movieId={movie.apiId ? movie.apiId : movie.id}
+            movie={movie}
           />
         </button>
       </div>
     </S.MovieTile>
-      </animated.div>
   );
 };
