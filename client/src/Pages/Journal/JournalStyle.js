dc419ce405e3979e39dd1ccf93870dcf676d310f
Yu <yw3028@tc.columbia.edu>
2020-10-19 10:49:25 +0200

    style: journal page style consistent with home page

--- client/src/Pages/Journal/JournalStyle.js
+++ client/src/Pages/Journal/JournalStyle.js
@@ -1,5 +1,12 @@
 import styled from 'styled-components/macro';
 
 export const JournalPage = styled.div`
-  margin: 20px 50px;
+  margin: 40px 8px;
+
+  h2 {
+    font-size: 1.5rem;
+    font-weight: 700;
+    margin: 1rem;
+    padding-left: 2rem;
+  }
 `;
