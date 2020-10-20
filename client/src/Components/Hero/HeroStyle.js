f63d9ba290ab81c2d0358bf77b24759d7aa00427
Jorge Macia <jmaciasalord@gmail.com>
2020-10-20 16:33:58 +0200

    fix: hide hero in mobile and scrollbars

--- client/src/Components/Hero/HeroStyle.js
+++ client/src/Components/Hero/HeroStyle.js
@@ -4,4 +4,8 @@ export const Hero = styled.div`
   height: 450px;
   margin: 0 3rem 4rem 3rem;
   padding: 0;
+  display: none;
+  @media screen and (min-device-width: 780px) {
+    display: block;
+  }
 `;
