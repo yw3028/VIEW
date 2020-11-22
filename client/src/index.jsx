0b84f62eded20b843b9c8a92e81df58286fc37a1
Yu <yw3028@tc.columbia.edu>
2020-11-22 12:32:04 +0100

    feat: create redux provider

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './Redux/reducer';
 import { BrowserRouter } from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
