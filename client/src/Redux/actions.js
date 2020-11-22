0db4431dd24b08ff9343241d422f69769d6dfa69
Yu <yw3028@tc.columbia.edu>
2020-11-22 12:12:11 +0100

    feat: add actions

--- client/src/Redux/actions.js
+++ client/src/Redux/actions.js
@@ -4,3 +4,38 @@ export const setMovies = (movies) => {
     payload: movies,
   };
 };
+
+export const setExplore = (movies) => {
+  return {
+    type: 'SET_EXPLORE',
+    payload: movies,
+  };
+};
+
+export const updateWishlist = (movieApiId) => {
+  return {
+    type: 'UPDATE_WISHLIST',
+    payload: movieApiId,
+  };
+};
+
+export const updateWatched = (movieApiId) => {
+  return {
+    type: 'UPDATE_WATCHED',
+    payload: movieApiId,
+  };
+};
+
+export const setJournal = (movieId) => {
+  return {
+    type: 'SET_JOURNAL',
+    payload: movieId,
+  };
+};
+
+export const removeJournal = (movieId) => {
+  return {
+    type: 'REMOVE_JOURNAL',
+    payload: movieId,
+  };
+};
